725dff9f6029a21fb2f6d4fb025e94e9c47b89f4
joao1barbosa <joao1.barbosa@outlook.com>
2025-03-02 15:34:00 -0300

    refactor: nvm instal

--- auto-install/zorin_os.sh
+++ auto-install/zorin_os.sh
@@ -149,9 +149,10 @@ install_nvm(){
 
   echo -e "${BLUE}[INFO] - Instalando NVM e Node.js (lts)${NON_COLOR}"
 
-  wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
 
-  source ~/.bashrc
+  export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+    [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 
   nvm install node
 }
