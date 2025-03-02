20e015357015c840f572d2a00d4e38b69af02dc7
joao1barbosa <joao1.barbosa@outlook.com>
2025-03-02 13:09:38 -0300

    feat: added docker download and config

--- zorin_os/auto-install.sh
+++ zorin_os/auto-install.sh
@@ -9,6 +9,8 @@ set -e
 ##URLS
 
 URL_DISCORD="https://discord.com/api/download?platform=linux&format=deb"
+URL_VSCODE="https://vscode.download.prss.microsoft.com/dbazure/download/stable/e54c774e0add60467559eb0d1e229c6452cf8447/code_1.97.2-1739406807_amd64.deb"
+URL_WARP="https://app.warp.dev/get_warp\?package\=deb"
 
 
 DIRETORIO_DOWNLOADS="$HOME/Downloads/programas"
@@ -58,11 +60,9 @@ sudo apt update -y
 
 PROGRAMAS_PARA_INSTALAR=(
   vlc
-  code
   folder-color
   git
   wget
-  ubuntu-restricted-extras
 )
 
 # ---------------------------------------------------------------------- #
@@ -74,7 +74,13 @@ install_debs(){
 echo -e "${BLUE}[INFO] - Baixando pacotes .deb${NON_COLOR}"
 
 mkdir -p "$DIRETORIO_DOWNLOADS"
-wget --content-disposition -c "$URL_DISCORD"       -P "$DIRETORIO_DOWNLOADS"
+cd "$DIRETORIO_DOWNLOADS"
+
+wget --content-disposition -c "$URL_DISCORD"
+wget --content-disposition -c "$URL_VSCODE"
+wget -c "$URL_WARP" -O warp-terminal-latest.deb
+
+cd "$HOME"
 
 ## Instalando pacotes .deb baixados na sessão anterior ##
 echo -e "${BLUE}[INFO] - Instalando pacotes .deb baixados${NON_COLOR}"
@@ -82,9 +88,6 @@ sudo dpkg -i $DIRETORIO_DOWNLOADS/*.deb
 
 # Instalar programas no apt
 echo -e "${BLUE}[INFO] - Instalando pacotes apt do repositório${NON_COLOR}"
-
-# Aceita automaticamente a licença das fontes da Microsoft
-echo "ttf-mscorefonts-installer msttcorefonts/accepted-mscorefonts-eula select true" | sudo debconf-set-selections
 
 for nome_do_programa in ${PROGRAMAS_PARA_INSTALAR[@]}; do
   if ! dpkg -l | grep -q $nome_do_programa; then # Só instala se já não estiver instalado
@@ -104,6 +107,32 @@ flatpak install flathub com.obsproject.Studio -y
 flatpak install flathub org.gimp.GIMP -y
 flatpak install flathub com.spotify.Client -y
 flatpak install flathub org.telegram.desktop -y
+flatpak install flathub io.dbeaver.DBeaverCommunity -y
+
+}
+
+##Instalando Docker ##
+install_docker(){
+
+# Remove pacotes conflitantes
+for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
+
+# Adicionar a chave GPG oficial do Docker:
+sudo apt-get update
+sudo apt-get install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Adicionar o repositório as fontes Apt:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+
+# Instalando definitivamente docker e extensões
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 }
 
@@ -149,6 +178,8 @@ travas_apt
 just_apt_update
 install_debs
 install_flatpaks
+extra_config
+install_docker
 apt_update
 system_clean
 
